94e7f0de0734a5025748817c516e47b6071fd3b3
Melinda Chang <melindachang.hy@gmail.com>
2022-09-24 14:49:19 -0700

    add: auto populating sidebar

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,34 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
-let dataScroll = new Set([
-  ...document.querySelectorAll('.header > *'),
-  ...document.querySelectorAll('.container-text > *'),
-  ...document.querySelectorAll('.divider'),
-  ...document.querySelectorAll('.img-box'),
-  ...document.querySelectorAll('iframe'),
-]);
+window.addEventListener('load', () => {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+  });
+  
+  let ul = document.querySelector('#sidebar ul');
 
-dataScroll.forEach((el) => el.setAttribute('data-scroll', 'true'));
+  document.querySelectorAll('.project .header h1').forEach((el) => {
+    let item = document.createElement('li');
+    let itemLink = document.createElement('a');
+    itemLink.textContent = el.textContent;
+    itemLink.addEventListener('click', () => {
+      scroll.scrollTo(el, { offset: '-60px' });
+    });
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-});
+    item.appendChild(itemLink);
+    ul.appendChild(item);
+  });
 
-document.querySelectorAll('.container-text a').forEach((link) => {
-  link.innerHTML =
-    '<div><span>' + link.textContent.trim().split('').join('</span><span>') + '</span></div>';
-  link
-    .querySelectorAll('span')
-    .forEach((s) => (s.innerHTML = s.textContent == ' ' ? '&nbsp;' : s.textContent));
-  link.insertAdjacentHTML(
-    'beforeend',
-    '<div><svg preserveAspectRatio="none" viewBox="0 0 192 5"><path d="M191.246 4H129C129 4 127.781 4.00674 127 4C114.767 3.89447 108.233 1 96 1C83.7669 1 77.2327 3.89447 65 4C64.219 4.00674 63 4 63 4H0.751923" /></svg></div>'
-  );
+  document.querySelectorAll('.container-text a').forEach((link) => {
+    link.innerHTML =
+      '<div><span>' + link.textContent.trim().split('').join('</span><span>') + '</span></div>';
+    link
+      .querySelectorAll('span')
+      .forEach((s) => (s.innerHTML = s.textContent == ' ' ? '&nbsp;' : s.textContent));
+    link.insertAdjacentHTML(
+      'beforeend',
+      '<div><svg preserveAspectRatio="none" viewBox="0 0 192 5"><path d="M191.246 4H129C129 4 127.781 4.00674 127 4C114.767 3.89447 108.233 1 96 1C83.7669 1 77.2327 3.89447 65 4C64.219 4.00674 63 4 63 4H0.751923" /></svg></div>'
+    );
+  });
 });

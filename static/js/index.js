8ba5dc0b3f4458cfa3f2219d73de16f3aba0a74e
Melinda Chang <32805643+melindachang@users.noreply.github.com>
2022-10-10 11:14:50 -0700

    fix: leftover merge issues

--- static/js/index.js
+++ static/js/index.js
@@ -1,48 +1,50 @@
-import LocomotiveScroll from 'locomotive-scroll';
+import LocomotiveScroll from "locomotive-scroll";
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
+    el: document.querySelector("[data-scroll-container]"),
     smooth: true,
   });
 
-  let nav = document.querySelector('nav');
+  let nav = document.querySelector("nav");
 
   let lastScroll = 0;
 
-  scroll.on('scroll', (instance) => {
-    instance.scroll.y > 100 ? nav.classList.add('filled') : nav.classList.remove('filled');
+  scroll.on("scroll", (instance) => {
+    instance.scroll.y > 100
+      ? nav.classList.add("filled")
+      : nav.classList.remove("filled");
     if (instance.scroll.y < lastScroll) {
-      nav.style.top = '0';
+      nav.style.top = "0";
     } else {
-      nav.style.top = '-8rem';
+      nav.style.top = "-8rem";
     }
 
     lastScroll = instance.scroll.y;
   });
 
-  scroll.on('call', (func) => {
+  const animationDuration = 1500;
+  const frameDuration = 1000 / 60;
+  const totalFrames = Math.round(animationDuration / frameDuration);
+
+  const easeOutQuad = (t) => t * (2 - t);
+  const animateCountUp = (el) => {
+    let frame = 0;
+    const countTo = parseInt(el.innerHTML, 10);
+    const counter = setInterval(() => {
+      frame++;
+      const progress = easeOutQuad(frame / totalFrames);
+      const currentCount = Math.round(countTo * progress);
+      if (parseInt(el.innerHTML, 10) !== currentCount) {
+        el.innerHTML = currentCount;
+      }
+      if (frame === totalFrames) {
+        clearInterval(counter);
+      }
+    }, frameDuration);
+  };
+
+  scroll.on("call", (func) => {
     eval(func);
   });
 });
-
-const animationDuration = 1500;
-const frameDuration = 1000 / 60;
-const totalFrames = Math.round(animationDuration / frameDuration);
-
-const easeOutQuad = (t) => t * (2 - t);
-const animateCountUp = (el) => {
-  let frame = 0;
-  const countTo = parseInt(el.innerHTML, 10);
-  const counter = setInterval(() => {
-    frame++;
-    const progress = easeOutQuad(frame / totalFrames);
-    const currentCount = Math.round(countTo * progress);
-    if (parseInt(el.innerHTML, 10) !== currentCount) {
-      el.innerHTML = currentCount;
-    }
-    if (frame === totalFrames) {
-      clearInterval(counter);
-    }
-  }, frameDuration);
-};
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,26 +1,29 @@
-let dropdowns = document.querySelectorAll('.dropdown');
-let submenus = document.querySelectorAll('.submenu');
+let dropdowns = document.querySelectorAll(".dropdown");
+let submenus = document.querySelectorAll(".submenu");
 
-window.addEventListener('click', (e) => {
-  if (!e.target.classList.contains('submenu') && !e.target.classList.contains('dropdown')) {
+window.addEventListener("click", (e) => {
+  if (
+    !e.target.classList.contains("submenu") &&
+    !e.target.classList.contains("dropdown")
+  ) {
     dropdowns.forEach((item) => {
-      item.parentElement.querySelector('.submenu').classList.remove('open');
+      item.parentElement.querySelector(".submenu").classList.remove("open");
     });
   }
 });
 
 dropdowns.forEach((el) =>
-  el.addEventListener('click', () => {
-    let submenu = el.parentElement.querySelector('.submenu');
+  el.addEventListener("click", () => {
+    let submenu = el.parentElement.querySelector(".submenu");
 
-    if (!submenu.classList.contains('open')) {
+    if (!submenu.classList.contains("open")) {
       submenus.forEach((item) => {
-        item.classList.remove('open');
-        item.style.transition = 'all .5s ease';
+        item.classList.remove("open");
+        item.style.transition = "all .5s ease";
       });
-      submenu.classList.add('open');
+      submenu.classList.add("open");
     } else {
-      submenu.classList.remove('open');
+      submenu.classList.remove("open");
     }
   })
 );

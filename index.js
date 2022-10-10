2ba7538360f19fa0bb1adb670b7bc42e6ef6c4ec
Melinda Chang <32805643+melindachang@users.noreply.github.com>
2022-10-10 11:30:38 -0700

    avoid vite minification for count function

--- index.js
+++ index.js
@@ -23,29 +23,27 @@ window.addEventListener("load", () => {
     lastScroll = instance.scroll.y;
   });
 
-  const animationDuration = 1500;
-  const frameDuration = 1000 / 60;
-  const totalFrames = Math.round(animationDuration / frameDuration);
-
-  const easeOutQuad = (t) => t * (2 - t);
-  const animateCountUp = (el) => {
-    let frame = 0;
-    const countTo = parseInt(el.innerHTML, 10);
-    const counter = setInterval(() => {
-      frame++;
-      const progress = easeOutQuad(frame / totalFrames);
-      const currentCount = Math.round(countTo * progress);
-      if (parseInt(el.innerHTML, 10) !== currentCount) {
-        el.innerHTML = currentCount;
-      }
-      if (frame === totalFrames) {
-        clearInterval(counter);
-      }
-    }, frameDuration);
-  };
-
   scroll.on("call", (func) => {
-    eval(func);
+    if (func === `animateCountUp(document.querySelector('#counter'))`) {
+      const animationDuration = 1500;
+      const frameDuration = 1000 / 60;
+      const totalFrames = Math.round(animationDuration / frameDuration);
+
+      const easeOutQuad = (t) => t * (2 - t);
+      let frame = 0;
+      const countTo = parseInt(el.innerHTML, 10);
+      const counter = setInterval(() => {
+        frame++;
+        const progress = easeOutQuad(frame / totalFrames);
+        const currentCount = Math.round(countTo * progress);
+        if (parseInt(el.innerHTML, 10) !== currentCount) {
+          el.innerHTML = currentCount;
+        }
+        if (frame === totalFrames) {
+          clearInterval(counter);
+        }
+      }, frameDuration);
+    }
   });
 
   let ul = document.querySelector("#sidebar ul");
